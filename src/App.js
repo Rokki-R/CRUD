1a71766c0e2fb1bdbd2705c4aa1d3f427bfb1dbe
Rokki-R <qvertas@gmail.com>
2023-10-25 21:06:45 +0300

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './HomePage';
-
+import Navbar from './Navbar';
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Switch>
         <Route path="/" component={HomePage} />
         {/* Add other routes as needed */}
